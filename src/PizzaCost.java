20750e9f39133b73213a0427e10ae80fa15ba641
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-12 13:03:27 -0400

    PizzaCost

--- src/PizzaCost.java
+++ src/PizzaCost.java
@@ -17,8 +17,7 @@ public class PizzaCost {
         totalcost = LABOUR + HEAT + (toppings * PERTOP) + (size * PERINCH);
         System.out.format("Total cost of your pizza is $%.2f\n", totalcost);
         System.out.println("I am full now!");
-        System.out.println("This was done at school");
-        System.out.println("This was done at Home again");
+        
         
     }
     
