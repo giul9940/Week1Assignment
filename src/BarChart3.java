b4005aef3872920076455b011e094e4633e6fe8b
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-13 13:15:11 -0400

    Work done at School

--- src/BarChart3.java
+++ src/BarChart3.java
@@ -1,10 +1,28 @@
+import TurtleGraphics.Pen;
+import TurtleGraphics.StandardPen;
+import TurtleGraphics.SketchPadWindow;
+import java.awt.Color;
 import java.util.Scanner;
 
 public class BarChart3 {
     
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
+        Pen p = new StandardPen(new SketchPadWindow(800,600));
+        PenBar b1 = new PenBar(), b2 = new PenBar(), b3 = new PenBar(), b4 = new PenBar();
+        System.out.print("Enter the width for bar 1: ");
+        b1.width = s.nextInt();
+        System.out.print("Enter the width for bar 2: ");
+        b2.width = s.nextInt();
+        System.out.print("Enter");
     }
+    
+    public static void makeBar(Pen p, PenBar pb, int x, int y, int w, Color c){
+        pb.x = x;
+        pb.y = y;
+       
+        pb.barColor = c;
+    }
+    
     
 }
---
+++ src/PenBar.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+
+
+public class PenBar {
+    int x, y;
+    int width;
+    Color barColor;
+    
+}
