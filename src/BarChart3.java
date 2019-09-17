7f8e7825fb35ebe1cf8829ddd51be79aa3985110
giul9940 <giul9940@ISS2V0SKH2.scdsb.pri>
2019-09-17 13:15:12 -0400

    BarChart3 and PenBar

--- src/BarChart3.java
+++ src/BarChart3.java
@@ -8,21 +8,31 @@ public class BarChart3 {
     
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        Pen p = new StandardPen(new SketchPadWindow(800,600));
         PenBar b1 = new PenBar(), b2 = new PenBar(), b3 = new PenBar(), b4 = new PenBar();
         System.out.print("Enter the width for bar 1: ");
         b1.width = s.nextInt();
         System.out.print("Enter the width for bar 2: ");
         b2.width = s.nextInt();
-        System.out.print("Enter");
+        System.out.print("Enter the width for bar 3: ");
+        b3.width = s.nextInt();
+        System.out.print("Enter the width for bar 4: ");
+        b4.width = s.nextInt();
+        
+        Pen p = new StandardPen(new SketchPadWindow(800,600));
+       
+        makeBar(p, b1, -380, 200, b1.width, Color.green);
+        makeBar(p, b2, -380, 100, b2.width, Color.blue);
+        makeBar(p, b3, -380, 0, b3.width, Color.yellow);
+        makeBar(p, b4, -380, -100, b4.width, Color.orange);
     }
     
     public static void makeBar(Pen p, PenBar pb, int x, int y, int w, Color c){
         pb.x = x;
-        pb.y = y;
-       
+        pb.y = y;       
         pb.barColor = c;
+        pb.draw(p);
     }
+    
     
     
 }
